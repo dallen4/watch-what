faf7211a6eb42cf667a7f763b5c615e118827d83
Nieky Allen <nieky.allen@gmail.com>
2022-04-08 23:31:43 -0500

    start imdb type

---
+++ src/types/imdb.ts
@@ -0,0 +1,18 @@
+export type IMDbTitle = {
+    Country: string;
+  Director: string;
+  Actors: string;
+  Writers: string;
+  Genres: string;
+  Title: string;
+  Plot: string;
+  Year: string;
+  Release: string;
+  Rating: string;
+  Votes: number;
+  Backdrop: string;
+  Poster: string;
+  Trailer_id: string;
+  Type: string;
+  Status: boolean;
+};
