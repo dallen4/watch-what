47a48cf7c5e92008370652be520ff66f74f1199d
Nieky Allen <nieky.allen@gmail.com>
2022-04-08 23:31:07 -0500

    update BrandLogo

--- src/atoms/BrandLogo/BrandLogo.tsx
+++ src/atoms/BrandLogo/BrandLogo.tsx
@@ -3,13 +3,16 @@ import clsx from 'clsx';
 import useStyles from './styles';
 import BaseLink from 'atoms/BaseLink';
 import Image from 'next/image';
+import { Typography } from '@material-ui/core';
 
 const BrandLogo = (props: BrandLogoPropTypes) => {
     const classes = useStyles();
 
     return (
         <BaseLink href={'/'}>
+            <Typography variant={'h4'}>
             Watch What?
+            </Typography>
         </BaseLink>
     );
 };
