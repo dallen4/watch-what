c9ccc52141c155d54e9fcfe9986a594c10a6b740
Nieky Allen <nieky.allen@gmail.com>
2022-04-10 22:30:43 -0500

    fetch details from tmdb

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,18 +1,36 @@
 import type { NextApiResponse } from 'next';
-import { SearchRequest } from 'types/general';
+import { SearchRequest, TitleWithDetails } from 'types/general';
 import { searchTitles } from '@lib/client';
+import { getTitleDetails } from '@lib/imdb';
 
-export default async function search(
-    req: SearchRequest,
-    res: NextApiResponse,
-) {
+export default async function search(req: SearchRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         res.status(405).end();
         return;
     }
 
-    const sources = await searchTitles(req.body);
+    const data = await searchTitles(req.body);
 
-    res.status(200).json(sources);
+    const titlesWithMetaReqs = data.titles.map(async (title) => {
+        const info: TitleWithDetails = { ...title };
+
+        if (title.imdb_id) {
+            const imdbDetails = await getTitleDetails(title.imdb_id);
+
+            info.details = {
+                ...imdbDetails,
+                poster: imdbDetails.poster_path
+                    ? `https://image.tmdb.org/t/p/w300_and_h450_bestv2` +
+                      imdbDetails.poster_path
+                    : undefined,
+            };
+        }
+
+        return info;
+    });
+
+    const titlesWithMeta = await Promise.all(titlesWithMetaReqs);
+
+    res.status(200).json({ titles: titlesWithMeta });
     return;
 }
