f6e19a0f19c551e61f6cfe4d3bc8ee678911ebed
Nieky Allen <nieky.allen@gmail.com>
2022-04-11 18:24:34 -0500

    setup client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,36 +1,11 @@
 import axios from 'axios';
-import { SearchInput, SearchResult } from 'types/general';
+import Session from "supertokens-auth-react/recipe/session";
 
-export const client = axios.create({
-    baseURL: 'https://api.watchmode.com',
-    params: {
-        apiKey: process.env.WATCHMODE_API_KEY,
-    },
-});
+const client = axios.create();
+Session.addAxiosInterceptors(client);
 
-export const getSources = async () => {
-    const response = await client.get('/v1/sources/');
-    return response.data;
-};
-
-export const getGenres = async () => {
-    const response = await client.get('/v1/genres/');
-    return response.data;
-};
-
-export const searchTitles = async (options: SearchInput) => {
-    const { query, sources, types, genres, sort_by } = options;
-
-    const response = await client.get<SearchResult>('/v1/list-titles/', {
-        params: {
-            source_ids: sources.join(','),
-            genres: genres.join(','),
-            types: types.join(','),
-            sort_by,
-            regions: 'US',
-            limit: 50,
-        },
-    });
-
-    return response.data;
+export const getSession = async () => {
+    const session = await client.get('/api/me');
+    console.log(session);
+    return session.data;
 };
