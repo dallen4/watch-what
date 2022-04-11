3658de888937aa95e3ca1ceeef07b748e734392e
Nieky Allen <nieky.allen@gmail.com>
2022-04-10 22:31:00 -0500

    implement tmdb requests

--- src/lib/imdb.ts
+++ src/lib/imdb.ts
@@ -1,12 +1,34 @@
+import axios from 'axios';
 import IMDb from 'imdb-light';
 import { IMDbTitle } from 'types/imdb';
 
+const client = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    params: {
+        api_key: '0e8df11290dfd54a54e5c5fee1845a1d',
+    },
+});
+
 export const getTitleDetails = async (titleId: string) => {
-    const request = new Promise<IMDbTitle>(function (resolve, reject) {
-        IMDb.fetch(titleId, resolve);
+    const request = await client.get(`/find/${titleId}`, {
+        params: {
+            external_source: 'imdb_id',
+        },
     });
 
-    const title = await request;
+    const {
+        movie_results,
+        tv_results,
+        tv_episode_results,
+        tv_season_results,
+    } = request.data;
 
-    return title;
+    const [details] = [
+        ...movie_results,
+        ...tv_results,
+        ...tv_episode_results,
+        ...tv_season_results,
+    ];
+
+    return details as IMDbTitle;
 };
