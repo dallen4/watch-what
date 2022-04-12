507865da76dc8c00747b33cc4f20c94ff3db609d
Nieky Allen <nieky.allen@gmail.com>
2022-04-11 19:18:21 -0500

    update frontend connfig

--- src/config/auth/frontend.ts
+++ src/config/auth/frontend.ts
@@ -1,6 +1,7 @@
 import { SuperTokensConfig } from 'supertokens-auth-react/lib/build/types';
 import PasswordlessReact from 'supertokens-auth-react/recipe/passwordless';
 import SessionReact from 'supertokens-auth-react/recipe/session';
+import theme from 'theme';
 import { appInfo } from './appInfo';
 
 export const frontendConfig = (): SuperTokensConfig => {
@@ -9,6 +10,29 @@ export const frontendConfig = (): SuperTokensConfig => {
         recipeList: [
             PasswordlessReact.init({
                 contactMethod: 'EMAIL',
+                palette: {
+                    background: theme.palette.background.default,
+                    error: theme.palette.error.main,
+                    textTitle: theme.palette.primary.main,
+                    textLabel: theme.palette.secondary.main,
+                    textInput: theme.palette.text.primary,
+                    textPrimary: theme.palette.primary.main,
+                    textLink: '#a9a9a9',
+                    inputBackground: theme.palette.grey[300],
+                },
+                signInUpFeature: {
+                    emailOrPhoneFormStyle: {
+                        button: {
+                            backgroundColor: theme.palette.primary.main,
+                            border: '0px',
+                            width: '100%',
+                            margin: '0 auto',
+                        },
+                        input: {
+
+                        },
+                    },
+                },
             }),
             SessionReact.init(),
         ],
