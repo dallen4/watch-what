6f8bd08c8cc99c23e56443a5fd0ef3f42b3dc641
Nieky Allen <nieky.allen@gmail.com>
2022-04-28 20:30:01 -0500

    create getStripe util

---
+++ src/lib/payments.ts
@@ -0,0 +1,16 @@
+/**
+ * This is a singleton to ensure we only instantiate Stripe once.
+ * ref: https://github.com/vercel/next.js/blob/canary/examples/with-stripe-typescript/utils/get-stripejs.ts
+ */
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+
+const getStripe = () => {
+    if (!stripePromise)
+        stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY!);
+
+    return stripePromise;
+};
+
+export default getStripe;
