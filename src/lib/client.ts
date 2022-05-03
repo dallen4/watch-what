c9a052d420086cb32b53a37dcd60ab03944d1d1f
Nieky Allen <nieky.allen@gmail.com>
2022-05-03 13:36:40 -0500

    remove log

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,10 +4,9 @@ import Session from 'supertokens-auth-react/recipe/session';
 const client = axios.create();
 Session.addAxiosInterceptors(client);
 
-export const getSession = async () => {
-    const session = await client.get('/api/me');
-    console.log(session);
-    return session.data;
+export const get = async (url: string) => {
+    const response = await client.get(url);
+    return response.data;
 };
 
 export const getCheckoutStatus = async (id: string) => {
