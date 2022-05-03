61071c8622fa41e3277ae3ac7e5006abf50e513b
Nieky Allen <nieky.allen@gmail.com>
2022-05-03 13:36:34 -0500

    remmove log

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,7 +7,6 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 let stripePromise: Promise<Stripe | null>;
 
 const getStripe = () => {
-    console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
     if (!stripePromise)
         stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 
