f08cce97149f6b667f92c930aa5c6f8bb3b71e8e
Nieky Allen <nieky.allen@gmail.com>
2022-05-03 01:39:55 -0500

    implement authentication

--- src/pages/api/checkout-sessions/index.ts
+++ src/pages/api/checkout-sessions/index.ts
@@ -4,11 +4,15 @@ import Stripe from 'stripe';
 import { superTokensNextWrapper } from 'supertokens-node/nextjs';
 import { verifySession } from 'supertokens-node/recipe/session/framework/express';
 import { ApiRequest } from 'types/general';
+import supertokens from 'supertokens-node';
+import { backendConfig } from '@config/auth/backend';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: '2020-08-27',
 });
+
+supertokens.init(backendConfig());
 
 export default async function handler(req: ApiRequest, res: NextApiResponse & Response) {
     if (req.method !== 'POST') {
@@ -31,6 +35,7 @@ export default async function handler(req: ApiRequest, res: NextApiResponse & Re
         // create Checkout Session
         const params: Stripe.Checkout.SessionCreateParams = {
             submit_type: 'pay',
+            mode: 'payment',
             payment_method_types: ['card'],
             line_items: [
                 {
@@ -41,14 +46,15 @@ export default async function handler(req: ApiRequest, res: NextApiResponse & Re
             metadata: {
                 userId,
             },
-            success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${req.headers.origin}/checkout`,
+            success_url: `${req.headers.origin}/premium/result?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${req.headers.origin}/premium/result`,
         };
 
         const checkoutSession = await stripe.checkout.sessions.create(params);
 
         res.status(200).json(checkoutSession);
     } catch (err) {
+        console.error(err);
         const errorMessage = err instanceof Error ? err.message : 'Internal server error';
         res.status(500).json({ message: errorMessage });
     }
