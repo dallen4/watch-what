898e48056f930a995ce08fea3388b2bd15a95273
Nieky Allen <nieky.allen@gmail.com>
2022-05-03 13:36:04 -0500

    add redis prefix

--- src/api/redis.ts
+++ src/api/redis.ts
@@ -1,5 +1,7 @@
 import Redis from 'ioredis';
 
-const client = new Redis(process.env.REDIS_URL!);
+const client = new Redis(process.env.REDIS_URL!, {
+    keyPrefix: 'watch-what:',
+});
 
 export default client;
