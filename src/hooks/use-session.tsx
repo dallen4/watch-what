a4493501515861ce8bdd6ff20efca0694803a4e4
Nieky Allen <nieky.allen@gmail.com>
2022-05-03 16:50:33 -0500

    remove local get

--- src/hooks/use-session.tsx
+++ src/hooks/use-session.tsx
@@ -1,17 +1,5 @@
+import { get } from '@lib/client';
 import useSWR from 'swr';
-
-const get = async (path: string) => {
-    const res = await fetch(path, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-
-    const data = await res.json();
-
-    return data;
-};
 
 export default function useSession() {
     const { data, error } = useSWR('/api/me', get);
