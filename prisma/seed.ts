98b4569e51bf4ea7ec44b8ae367ab2bfe287e7c1
Nieky Allen <nieky.allen@gmail.com>
2022-05-11 23:47:40 -0500

    update prisma seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,34 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { getUsersOldestFirst } from 'supertokens-node';
-import { init } from '../src/api/auth';
+import { generateUsername } from '../src/lib/util';
+import * as Auth from '../src/api/auth';
 
-init();
+Auth.init();
 
 const prisma = new PrismaClient();
 
 async function main() {
-    console.log(`Start seeding ...`);
+    console.log(`Start seeding...`);
+
     const existingUsers = await getUsersOldestFirst();
 
     console.log(existingUsers.users);
+
+    const users: Prisma.UserCreateInput[] = existingUsers.users.map(({ user }) => {
+        const name = generateUsername(user.id);
+
+        return {
+            id: user.id,
+            name,
+            plan: 'FREE',
+        };
+    });
+
+    console.log('Generating users...');
+
+    await prisma.user.createMany({
+        data: users,
+    });
 
     console.log(`Seeding finished.`);
 }
