80beaa61a0f529edcfcddfff5466ddafd119ab64
Nieky Allen <nieky.allen@gmail.com>
2022-05-11 23:47:57 -0500

    add generateUsername

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,20 @@
 import crypto from 'crypto';
+import { nanoid } from 'nanoid';
+import { adjectives, animals, uniqueNamesGenerator } from 'unique-names-generator';
 
 export const hash = (input: any) => {
     return crypto.createHash('sha1').update(JSON.stringify(input)).digest('hex');
+};
+
+export const generateUsername = (prevSeed?: string) => {
+    const seed = prevSeed || nanoid();
+
+    const username = uniqueNamesGenerator({
+        dictionaries: [adjectives, animals],
+        separator: '',
+        style: 'capital',
+        seed,
+    });
+
+    return username;
 };
