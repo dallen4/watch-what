a1de8054680a76fbacdf7252ae270211ce2129e2
Nieky Allen <nieky.allen@gmail.com>
2022-07-30 01:25:52 -0500

    futher cleanup

--- src/api/data/watchmode.ts
+++ src/api/data/watchmode.ts
@@ -34,3 +34,23 @@ export const searchTitles = async (options: SearchInput) => {
 
     return response.data;
 };
+
+export const getTitleSources = async (titleId: number) => {
+    const response = await client.get<Source[]>(`/v1/title/${titleId}/sources/`);
+
+    return response.data;
+};
+
+type Source = {
+    source_id: number;
+    name: string;
+    type: string;
+    region: string;
+    ios_url?: string;
+    android_url?: string;
+    web_url?: string;
+    format: string;
+    price: number;
+    seasons?: number;
+    episodes?: number;
+};
